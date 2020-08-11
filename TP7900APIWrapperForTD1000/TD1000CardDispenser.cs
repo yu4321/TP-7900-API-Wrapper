aa668c84ffead891b1233ce907649192397fde70
Ryu Youngseok <ryu.youngseok0512@mediasol.kr>
2020-08-11 14:34:09 +0900

    TestProgram Added(한국어, 간단하게)

--- TP7900APIWrapperForTD1000/TD1000CardDispenser.cs
+++ TP7900APIWrapperForTD1000/TD1000CardDispenser.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TP7900APIWrapperForTD1000
 {
-    public class TD1000CardDispenser
+    public class TD1000CardDispenser : IDisposable
     {
         public Action<string> LoggingAction;
         public delegate void TP7900InsertedCardUidReceivedHandler(object sender, TP7900InsertedCardUidEventArgs e);
@@ -14,40 +14,173 @@ namespace TP7900APIWrapperForTD1000
 
         public bool IsInitialized { get; private set; }
 
+        private async Task<bool> BoolReturnPromise(Func<int> func)
+        {
+            var promise = new TaskCompletionSource<bool>();
+            await Task.Run(() =>
+            {
+                var res = func.Invoke();
+                promise.SetResult(res == 0);
+            });
+            return await promise.Task;
+        }
+
         public async Task<bool> Initialize()
         {
-
+            var openResult = await BoolReturnPromise(() => TP7900.OpenDevice(2, 0, 1));
+            if (openResult)
+            {
+                IsInitialized = true;
+                return await BoolReturnPromise(() => TP7900.InitDevice());
+            }
+            return false;
         }
 
-        public async Task<bool> WaitForInsertCard()
+        public void Close()
         {
-
+            IsInitialized = false;
+            Task.Run(() =>
+            {
+                TP7900.CloseDevice();
+            });
         }
 
-        public async Task<bool> StartWaitingForCardInsertion()
+        public async Task<bool> CanDispense()
         {
+            var pRailStatus = new byte[8];
+            var pFeedRollerStatus = new byte[8];
+            var pTraySensorStatus = new byte[8];
+            var statResult = await BoolReturnPromise(() => TP7900.GetSensorStatus(pRailStatus, pFeedRollerStatus, pTraySensorStatus));
+            if (statResult)
+            {
+                return pTraySensorStatus[0] == 2;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
+        public async Task<byte[]> WaitForInsertCard()
+        {
+            var insertResult= await BoolReturnPromise(() => TP7900.RF_Ready_Position(0x31, false));
+            if (!insertResult)
+            {
+                return null;
+            }
+
+            var data = new byte[100];
+            var size = new int[100];
+            var getValueResult = await BoolReturnPromise(() => TP7900.RFM_DUAL_Power(true, 0, data, size));
+
+            if (!getValueResult)
+            {
+                return null;
+            }
+
+            return data.Skip(1).Take(size[0] - 1).ToArray();
+        }
+
+        public async Task<byte[]> WaitForDispenseCard()
+        {
+            var insertResult = await BoolReturnPromise(() => TP7900.RF_Ready_Position(0x31, true));
+            if (!insertResult)
+            {
+                return null;
+            }
+
+            var data = new byte[100];
+            var size = new int[100];
+            var getValueResult = await BoolReturnPromise(() => TP7900.RFM_DUAL_Power(true, 0, data, size));
+
+            if (!getValueResult)
+            {
+                return null;
+            }
+
+            return data.Skip(1).Take(size[0] - 1).ToArray();
+        }
+
+        public void StartWaitingForCardInsertion()
+        {
+            Task.Run(async() =>
+            {
+                var insertResult = await BoolReturnPromise(() => TP7900.RF_Ready_Position(0x31, false));
+                if (!insertResult)
+                {
+                    UidReceived(this, null);
+                }
+
+                var data = new byte[100];
+                var size = new int[100];
+                var getValueResult = await BoolReturnPromise(() => TP7900.RFM_DUAL_Power(true, 0, data, size));
+
+                if (!getValueResult)
+                {
+                    UidReceived(this, new TP7900InsertedCardUidEventArgs(null));
+                }
+
+                UidReceived(this,new TP7900InsertedCardUidEventArgs(data.Skip(1).Take(size[0] - 1).ToArray()));
+            });
         }
 
         public async Task<bool> DispenseCurrentCard()
         {
-
+            return await BoolReturnPromise(() => TP7900.EjectCard());
         }
 
         public async Task<bool> RejectCurrentCard()
         {
-
+            return await BoolReturnPromise(() => TP7900.RejectCard());
         }
 
         public async Task<bool> AcceptCurrentCard()
         {
-
+            return await BoolReturnPromise(() => TP7900.RejectCard());
         }
 
         public async Task<bool> RevertCurrentCard()
         {
-
+            return await BoolReturnPromise(() => TP7900.EjectCard());
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // 중복 호출을 검색하려면
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (IsInitialized)
+                    {
+                        Close();
+                    }
+                }
+
+                // TODO: 관리되지 않는 리소스(관리되지 않는 개체)를 해제하고 아래의 종료자를 재정의합니다.
+                // TODO: 큰 필드를 null로 설정합니다.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 위의 Dispose(bool disposing)에 관리되지 않는 리소스를 해제하는 코드가 포함되어 있는 경우에만 종료자를 재정의합니다.
+        // ~TD1000CardDispenser() {
+        //   // 이 코드를 변경하지 마세요. 위의 Dispose(bool disposing)에 정리 코드를 입력하세요.
+        //   Dispose(false);
+        // }
+
+        // 삭제 가능한 패턴을 올바르게 구현하기 위해 추가된 코드입니다.
+        public void Dispose()
+        {
+            // 이 코드를 변경하지 마세요. 위의 Dispose(bool disposing)에 정리 코드를 입력하세요.
+            Dispose(true);
+            // TODO: 위의 종료자가 재정의된 경우 다음 코드 줄의 주석 처리를 제거합니다.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 
     public class TP7900InsertedCardUidEventArgs : EventArgs
---
+++ TesterProg/TesterViewModel.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TP7900APIWrapperForTD1000;
+
+namespace TesterProg
+{
+    public class TesterViewModel : INotifyPropertyChanged
+    {
+        private string _loadingText;
+        public string LoadingText
+        {
+            get
+            {
+                return _loadingText;
+            }
+            set
+            {
+                _loadingText = value;
+                RaisePropertyChanged(nameof(LoadingText));
+            }
+        }
+
+        private Visibility _loadingVisibility = Visibility.Collapsed;
+        public Visibility LoadingVisibility
+        {
+            get
+            {
+                return _loadingVisibility;
+            }
+            set
+            {
+                _loadingVisibility = value;
+                RaisePropertyChanged(nameof(LoadingVisibility));
+            }
+        }
+        private string log;
+        public string Log
+        {
+            get
+            {
+                return log;
+            }
+            set
+            {
+                log = value;
+                RaisePropertyChanged(nameof(Log));
+            }
+        }
+
+        public ICommand ConnectCommand { get; private set; }
+        public ICommand StartReceiveCommand { get; private set; }
+        public ICommand StartDispenseCommand { get; private set; }
+        public ICommand RejectCommand { get; private set; }
+        public ICommand SupplyCommand { get; private set; }
+
+        private string _receivedUid;
+        public string ReceivedUid
+        {
+            get
+            {
+                return _receivedUid;
+            }
+            set
+            {
+                _receivedUid = value;
+                RaisePropertyChanged(nameof(ReceivedUid));
+            }
+        }
+
+        TD1000CardDispenser Dispenser = new TD1000CardDispenser();
+
+        public TesterViewModel()
+        {
+            ConnectCommand = new RelayCommand<object>(async(x) =>
+              {
+                  LoadingVisibility = Visibility.Visible;
+                  LoadingText = "기기 초기화 작업중입니다!";
+                  var res = await Dispenser.Initialize();
+                  if (res)
+                  {
+                      WriteLog("접속 및 초기화 성공");
+                  }
+                  else
+                  {
+                      WriteLog("접속 및 초기화 실패");
+                  }
+                  LoadingVisibility = Visibility.Collapsed;
+              }, (x) =>
+              {
+                  return !Dispenser.IsInitialized;
+              });
+            StartReceiveCommand = new RelayCommand<object>(async (x) =>
+            {
+                LoadingVisibility = Visibility.Visible;
+                LoadingText = "카드를 넣어주세요!";
+                var res = await Dispenser.WaitForInsertCard();
+                if (res != null)
+                {
+                    ReceivedUid = BitConverter.ToString(res);
+
+                    var data1 = BitConverter.ToString(res.Reverse().ToArray()).Replace("-", string.Empty).ToUpper();
+                    var uid = Int64.Parse(data1, System.Globalization.NumberStyles.HexNumber).ToString();
+                    if (uid.Length < 10)
+                        uid = new string('0', 10 - uid.Length) + uid;
+                    WriteLog($"삽입 카드 UID 원문: {ReceivedUid} Decimal10DigitUid: {uid}");
+                }
+                else
+                {
+                    ReceivedUid = "";
+                    WriteLog("카드 삽입 감지 실패");
+                }
+                LoadingVisibility = Visibility.Collapsed;
+            }, (x) =>
+            {
+                return Dispenser.IsInitialized;
+            });
+
+            StartDispenseCommand = new RelayCommand<object>(async (x) =>
+            {
+                LoadingVisibility = Visibility.Visible;
+                LoadingText = "카드를 준비합니다";
+                var res = await Dispenser.WaitForDispenseCard();
+                if (res != null)
+                {
+                    ReceivedUid = BitConverter.ToString(res);
+
+                    var data1 = BitConverter.ToString(res.Reverse().ToArray()).Replace("-", string.Empty).ToUpper();
+                    var uid = Int64.Parse(data1, System.Globalization.NumberStyles.HexNumber).ToString();
+                    if (uid.Length < 10)
+                        uid = new string('0', 10 - uid.Length) + uid;
+                    WriteLog($"배출 준비 카드 UID 원문: \n---{ReceivedUid}---\n Decimal10DigitUid: \n---{uid}---");
+                }
+                else
+                {
+                    ReceivedUid = "";
+                    WriteLog("카드 방출 준비 실패");
+                }
+                LoadingVisibility = Visibility.Collapsed;
+            }, (x) =>
+            {
+                return Dispenser.IsInitialized;
+            });
+
+            RejectCommand = new RelayCommand<object>(async (x) =>
+            {
+                LoadingVisibility = Visibility.Visible;
+                LoadingText = "카드를 수납합니다";
+                var res = await Dispenser.RejectCurrentCard();
+                if (res)
+                {
+                    ReceivedUid = "";
+                    WriteLog($"카드 수납 성공");
+                }
+                else
+                {
+                    ReceivedUid = "";
+                    WriteLog("카드 수납 실패");
+                }
+                LoadingVisibility = Visibility.Collapsed;
+            }, (x) =>
+            {
+                return Dispenser.IsInitialized;
+            });
+
+            SupplyCommand = new RelayCommand<object>(async (x) =>
+            {
+                LoadingVisibility = Visibility.Visible;
+                LoadingText = "카드를 방출합니다";
+                var res = await Dispenser.DispenseCurrentCard();
+                if (res)
+                {
+                    ReceivedUid = "";
+                    WriteLog($"카드 방출 성공");
+                }
+                else
+                {
+                    ReceivedUid = "";
+                    WriteLog("카드 방출 실패");
+                }
+                LoadingVisibility = Visibility.Collapsed;
+            }, (x) =>
+            {
+                return Dispenser.IsInitialized;
+            });
+        }
+
+        private void WriteLog(string s)
+        {
+            Log += $"{DateTime.Now} - {s}\n";
+        }
+
+        #region Impl PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            // ISSUE: reference to a compiler-generated field
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged == null)
+                return;
+            propertyChanged((object)this, new PropertyChangedEventArgs(propertyName));
+        } 
+        #endregion
+    }
+}
