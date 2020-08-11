b6e916f8fd20d4b909b6da80c995fd442b5d93b3
Ryu Youngseok <ryu.youngseok0512@mediasol.kr>
2020-08-11 12:02:26 +0900

    초기 커밋.

---
+++ TP7900APIWrapperForTD1000/TD1000CardDispenser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP7900APIWrapperForTD1000
+{
+    public class TD1000CardDispenser
+    {
+        public Action<string> LoggingAction;
+        public delegate void TP7900InsertedCardUidReceivedHandler(object sender, TP7900InsertedCardUidEventArgs e);
+        public event TP7900InsertedCardUidReceivedHandler UidReceived;
+
+        public bool IsInitialized { get; private set; }
+
+        public async Task<bool> Initialize()
+        {
+
+        }
+
+        public async Task<bool> WaitForInsertCard()
+        {
+
+        }
+
+        public async Task<bool> StartWaitingForCardInsertion()
+        {
+
+        }
+
+        public async Task<bool> DispenseCurrentCard()
+        {
+
+        }
+
+        public async Task<bool> AcceptCurrentCard()
+        {
+
+        }
+    }
+
+    public class TP7900InsertedCardUidEventArgs : EventArgs
+    {
+        public TP7900InsertedCardUidEventArgs(byte[] receivedData)
+        {
+            ReceivedData = receivedData;
+        }
+
+        public byte[] ReceivedData { get; set; }
+
+    }
+}
---
+++ TP7900APIWrapperForTD1000/TP7900.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP7900APIWrapperForTD1000
+{
+    public class TP7900
+    {
+        [DllImport("TP7900.dll")]
+        public static extern int GetNumOfConnectedDevices(int nPortType, int[] pNumOfDevices);
+
+        [DllImport("TP7900.dll")]
+        public static extern int Get_DeviceName(int nIndex, StringBuilder pszDeviceName);
+
+        [DllImport("TP7900.dll")]
+        public static extern int OpenDevice(int ConnectionType, int iParam1, int iParam2);
+
+        [DllImport("TP7900.dll")]
+        public static extern int CloseDevice();
+
+        [DllImport("TP7900.dll")]
+        public static extern int InitDevice();
+
+        [DllImport("TP7900.dll")]
+        public static extern int Get_VersionInfo(StringBuilder InfoBuffer);
+
+        [DllImport("TP7900.dll")]
+        public static extern int Get_Status(string st1, string st2);
+
+        [DllImport("TP7900.dll")]
+        public static extern int SupplyCard(int nTrayNum);
+
+        [DllImport("TP7900.dll")]
+        public static extern int EjectCard();
+
+        [DllImport("TP7900.dll")]
+        public static extern int RejectCard();
+
+        [DllImport("TP7900.dll")]
+        public static extern int GetSensorStatus(byte[] pRailStatus, byte[] pFeedRollerStatus, byte[] pTraySensorStatus);
+
+        [DllImport("TP7900.dll")]
+        public static extern int RF_Ready_Position(byte byTrayNumber, bool bFront);
+
+        [DllImport("TP7900.dll")]
+        public static extern int RFM_CardCheck(int nAntenna, int nCardType);
+
+        [DllImport("TP7900.dll")]
+        public static extern int RFM_Power(int nAntenna, int nCardType, int nOnTime, byte[] pRxData, int[] pRxSize);
+
+        [DllImport("TP7900.dll")]
+        public static extern int RFM_Direct(int nAntenna, int nCardType, byte[] pTxData, int nTxSize, byte[] pRxData, int[] pRxSize);
+
+        [DllImport("TP7900.dll")]
+        public static extern int RFM_DUAL_Power(bool bIsPower, int CardType, byte[] pRxData, int[] pRxSize);
+
+    }
+}
